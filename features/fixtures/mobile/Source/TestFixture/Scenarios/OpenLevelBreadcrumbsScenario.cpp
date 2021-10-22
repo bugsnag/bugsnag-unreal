7940d9fa0b4dbd356685a43ee937dc8b633d1014
Delisa Mason <iskanamagus@gmail.com>
2021-10-22 15:32:49 +0100

    refactor: share test implementation of GetCurrentPlayWorld

--- features/fixtures/mobile/Source/TestFixture/Scenarios/OpenLevelBreadcrumbsScenario.cpp
+++ features/fixtures/mobile/Source/TestFixture/Scenarios/OpenLevelBreadcrumbsScenario.cpp
@@ -2,36 +2,6 @@
 
 #include "Kismet/GameplayStatics.h"
 
-// Because UEngine::GetCurrentPlayWorld() does not exist in 4.23 :-/
-static UWorld* GetCurrentPlayWorld()
-{
-	// This logic copied from FEngineService::SendPong() in Runtime/Engine/Private/EngineService.cpp
-	FWorldContext const* ContextToUse = nullptr;
-	if (GEngine)
-	{
-		// We're going to look through the WorldContexts and pull any Game context we find
-		// If there isn't a Game context, we'll take the first PIE we find
-		// and if none of those we'll use an Editor
-		for (const FWorldContext& WorldContext : GEngine->GetWorldContexts())
-		{
-			if (WorldContext.WorldType == EWorldType::Game)
-			{
-				ContextToUse = &WorldContext;
-				break;
-			}
-			else if (WorldContext.WorldType == EWorldType::PIE && (ContextToUse == nullptr || ContextToUse->WorldType != EWorldType::PIE))
-			{
-				ContextToUse = &WorldContext;
-			}
-			else if (WorldContext.WorldType == EWorldType::Editor && ContextToUse == nullptr)
-			{
-				ContextToUse = &WorldContext;
-			}
-		}
-	}
-	return ContextToUse ? ContextToUse->World() : nullptr;
-}
-
 class OpenLevelBreadcrumbsScenario : public Scenario
 {
 public:
