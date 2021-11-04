45329c88673ba67d545d025fca45ba187ccf29b5
Nick Dowell <nick@bugsnag.com>
2021-11-04 13:43:27 +0000

    Reimplement GetCurrentPlayWorld()

--- Plugins/Bugsnag/Source/Bugsnag/Private/BugsnagConfiguration.cpp
+++ Plugins/Bugsnag/Source/Bugsnag/Private/BugsnagConfiguration.cpp
@@ -196,31 +196,17 @@ TSharedPtr<FJsonValue> FBugsnagConfiguration::GetMetadata(const FString& Section
 // Because UEngine::GetCurrentPlayWorld() does not exist in 4.23 :-/
 static UWorld* GetCurrentPlayWorld()
 {
-	// This logic copied from FEngineService::SendPong() in Runtime/Engine/Private/EngineService.cpp
-	FWorldContext const* ContextToUse = nullptr;
 	if (GEngine)
 	{
-		// We're going to look through the WorldContexts and pull any Game context we find
-		// If there isn't a Game context, we'll take the first PIE we find
-		// and if none of those we'll use an Editor
-		for (const FWorldContext& WorldContext : GEngine->GetWorldContexts())
+		for (const FWorldContext& Context : GEngine->GetWorldContexts())
 		{
-			if (WorldContext.WorldType == EWorldType::Game)
+			if (Context.WorldType == EWorldType::Game)
 			{
-				ContextToUse = &WorldContext;
-				break;
-			}
-			else if (WorldContext.WorldType == EWorldType::PIE && (ContextToUse == nullptr || ContextToUse->WorldType != EWorldType::PIE))
-			{
-				ContextToUse = &WorldContext;
-			}
-			else if (WorldContext.WorldType == EWorldType::Editor && ContextToUse == nullptr)
-			{
-				ContextToUse = &WorldContext;
+				return Context.World();
 			}
 		}
 	}
-	return ContextToUse ? ContextToUse->World() : nullptr;
+	return nullptr;
 }
 
 void FBugsnagConfiguration::AddDefaultMetadata()
