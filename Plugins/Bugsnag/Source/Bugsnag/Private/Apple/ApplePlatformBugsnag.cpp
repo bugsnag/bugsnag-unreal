ae52bc2e8ad43c590b0e1bd979e0681eddb81ac7
Nick Dowell <nick@bugsnag.com>
2022-02-03 13:54:53 +0000

    Discard non-PIE Mac Editor crashes

--- Plugins/Bugsnag/Source/Bugsnag/Private/Apple/ApplePlatformBugsnag.cpp
+++ Plugins/Bugsnag/Source/Bugsnag/Private/Apple/ApplePlatformBugsnag.cpp
@@ -25,6 +25,23 @@
 
 DEFINE_PLATFORM_BUGSNAG(FApplePlatformBugsnag);
 
+#if UE_EDITOR
+
+#import <Bugsnag/BSG_KSCrashReportWriter.h>
+#import <BugsnagPrivate/BugsnagConfiguration+Private.h>
+
+#define IS_PIE_WORLD_KEY "isPlayInEditorWorld"
+#define UNREAL_ENGINE_KEY "unrealEngine"
+
+static void FApplePlatformBugsnag_OnCrashHandler(const BSG_KSCrashReportWriter* writer)
+{
+	writer->beginObject(writer, UNREAL_ENGINE_KEY);
+	writer->addBooleanElement(writer, IS_PIE_WORLD_KEY, GIsPlayInEditorWorld);
+	writer->endContainer(writer);
+}
+
+#endif
+
 void FApplePlatformBugsnag::Start(const TSharedRef<FBugsnagConfiguration>& Configuration)
 {
 	BugsnagConfiguration* CocoaConfig = FApplePlatformConfiguration::Configuration(Configuration);
@@ -33,6 +50,19 @@ void FApplePlatformBugsnag::Start(const TSharedRef<FBugsnagConfiguration>& Confi
 	{
 		UE_LOG(LogBugsnag, Log, TEXT("App hang detection is disabled in the Editor"));
 		CocoaConfig.enabledErrorTypes.appHangs = NO;
+
+		UE_LOG(LogBugsnag, Log, TEXT("Crashes in the Editor outside of a PIE session will not be reported"));
+		CocoaConfig.onCrashHandler = &FApplePlatformBugsnag_OnCrashHandler;
+		BugsnagOnSendErrorBlock OnSend = ^BOOL(BugsnagEvent* _Nonnull event) {
+			if ([[event getMetadataFromSection:@UNREAL_ENGINE_KEY withKey:@IS_PIE_WORLD_KEY] isEqual:@NO])
+			{
+				UE_LOG(LogBugsnag, Log, TEXT("Discarding crash in Editor outside of PIE session"));
+				return NO;
+			}
+			return YES;
+		};
+		// Directly accessing `onSendBlocks` because we need this block to run first
+		[CocoaConfig.onSendBlocks insertObject:OnSend atIndex:0];
 	}
 	else
 	{
