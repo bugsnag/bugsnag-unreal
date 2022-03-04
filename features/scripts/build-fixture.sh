61473b25e3ccc046e5ca5f7f60457edae9d5467a
Josh Edney <josh.edney@smartbear.com>
2022-03-04 11:55:13 +0000

    [full ci]

--- features/scripts/build-fixture.sh
+++ features/scripts/build-fixture.sh
@@ -51,7 +51,7 @@ if [ "${PLATFORM}" == "Mac" ]; then
 elif [[ "$PLATFORM" == "Android" ]]; then
   echo "--- Renaming Android test fixture"
   mv features/fixtures/generic/Binaries/Android/TestFixture-Android-Shipping-arm64.apk build/TestFixture-Android-Shipping-"$UE_VERSION"-arm64.apk
-  mv features/fixtures/generic/Binaries/Android/TestFixture-Android-Shipping-armv7.apk build/TestFixture-Android-Shipping-"$UE_VERSION"-armv7.apk
+#  mv features/fixtures/generic/Binaries/Android/TestFixture-Android-Shipping-armv7.apk build/TestFixture-Android-Shipping-"$UE_VERSION"-armv7.apk
 elif [[ "$PLATFORM" == "IOS" ]]; then
   echo "--- Renaming iOS test fixture"
   mv features/fixtures/generic/Binaries/IOS/TestFixture-IOS-Shipping.dSYM build/TestFixture-IOS-Shipping-"$UE_VERSION".dSYM
