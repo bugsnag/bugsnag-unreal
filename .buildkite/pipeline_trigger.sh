7c8fa79a86a4cbd4464bf44b6bf06dcebf039700
Steve Kirkland-Walton <steve@bugsnag.com>
2022-04-11 11:33:17 +0100

    Correct branch name for build triggers

--- .buildkite/pipeline_trigger.sh
+++ .buildkite/pipeline_trigger.sh
@@ -2,9 +2,9 @@
 
 if [[ "$BUILDKITE_MESSAGE" == *"[full ci]"* ||
   "$BUILDKITE_BRANCH" == "next" ||
-  "$BUILDKITE_BRANCH" == "master" ||
+  "$BUILDKITE_BRANCH" == "main" ||
   ! -z "$FULL_SCHEDULED_BUILD" ||
-  "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "master" ]]; then
+  "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "main" ]]; then
   echo "Running full build"
   buildkite-agent pipeline upload .buildkite/pipeline.full.yml
 fi
