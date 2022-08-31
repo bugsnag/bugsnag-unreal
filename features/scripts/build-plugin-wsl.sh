d99ee874b824e828b55ac554fd8cfbc4e373a354
Josh Edney <josh.edney@smartbear.com>
2022-08-31 12:27:36 +0100

    revert makefile changes

--- features/scripts/build-plugin-wsl.sh
+++ features/scripts/build-plugin-wsl.sh
@@ -6,4 +6,4 @@ UE_HOME="C:\Program Files\Epic Games\UE_$UE_VERSION"
 
 UE_RUNUAT="$UE_HOME\Engine\Build\BatchFiles\RunUAT.bat"
 
-/mnt/c/windows/system32/cmd.exe /C $UE_RUNUAT BuildPlugin -Plugin="$(wslpath -w .)/plugins/Bugsnag/Bugsnag.uplugin" -Package="$(wslpath -w .)/Build/Plugin/Bugsnag" -TargetPlatforms=Win32+Win64
+/mnt/c/windows/system32/cmd.exe /C "$UE_RUNUAT" BuildPlugin -Plugin="$(wslpath -w .)/plugins/Bugsnag/Bugsnag.uplugin" -Package="$(wslpath -w .)/Build/Plugin/Bugsnag" -TargetPlatforms=Win32+Win64
