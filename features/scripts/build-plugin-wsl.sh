9fa679b856bd3f78376f9f78f2bb3e06a3c78bd9
Josh Edney <josh.edney@smartbear.com>
2022-08-31 12:52:40 +0100

    add zip steps to windows wsl build script

--- features/scripts/build-plugin-wsl.sh
+++ features/scripts/build-plugin-wsl.sh
@@ -6,4 +6,10 @@ UE_HOME="C:\Program Files\Epic Games\UE_$UE_VERSION"
 
 UE_RUNUAT="$UE_HOME\Engine\Build\BatchFiles\RunUAT.bat"
 
+SCRIPT_DIR=$(pwd)
+
 /mnt/c/windows/system32/cmd.exe /C "$UE_RUNUAT" BuildPlugin -Plugin="$(wslpath -w .)/plugins/Bugsnag/Bugsnag.uplugin" -Package="$(wslpath -w .)/Build/Plugin/Bugsnag" -TargetPlatforms=Win32+Win64
+
+cd "$SCRIPT_DIR/Build/Plugin"
+
+zip -r" $SCRIPT_DIR/Bugsnag-UE$UE_VERSION-Windows.zip" Bugsnag
