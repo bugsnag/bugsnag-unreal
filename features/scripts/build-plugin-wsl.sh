657ca0798d2c915a211af6c75ae1f5ffdea9e426
Steve Kirkland-Walton <steve@bugsnag.com>
2022-09-07 22:17:28 +0100

    Debug

--- features/scripts/build-plugin-wsl.sh
+++ features/scripts/build-plugin-wsl.sh
@@ -5,11 +5,11 @@ set -Eeuo pipefail
 UE_VERSION="${UE_VERSION:-4.27}"
 
 UE_HOME="C:\Program Files\Epic Games\UE_$UE_VERSION"
-UE_RUNUAT="$UE_HOME/Engine/Build/BatchFiles/RunUAT.bat"
+UE_RUNUAT="$UE_HOME\Engine\Build\BatchFiles\RunUAT.bat"
 
 SCRIPT_DIR=$(pwd)
 
-echo "$UE_RUNUAT BuildPlugin -Plugin="$(wslpath -w .)/plugins/Bugsnag/Bugsnag.uplugin" -Package="$(wslpath -w .)/Build/Plugin/Bugsnag" -TargetPlatforms=Win32+Win64" > run_plugin.bat
+echo "\"$UE_RUNUAT\" BuildPlugin -Plugin="$(wslpath -w .)/plugins/Bugsnag/Bugsnag.uplugin" -Package="$(wslpath -w .)/Build/Plugin/Bugsnag" -TargetPlatforms=Win32+Win64" > run_plugin.bat
 
 cat run_plugin.bat
 
