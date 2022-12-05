f0fc5a0b6190e7a4c0e960db5e88bb09661ab3b7
Josh Edney <josh.edney@smartbear.com>
2022-12-05 11:14:24 +0000

    set ndk version via makefile

--- scripts/build-plugin.sh
+++ scripts/build-plugin.sh
@@ -4,8 +4,8 @@ set -a
 
 UE_VERSION="${UE_VERSION:-4.27}"
 
-if [ "$UE_VERSION" == 4.27]; then
-  NDKROOT=/Users/administrator/Library/Android/sdk/ndk/21.4.7075529
+if [[ "$UE_VERSION" == "4.27" ]]; then
+  NDKROOT="/Users/administrator/Library/Android/sdk/ndk/21.4.7075529"
 fi
 
 rm -rf "/Users/administrator/Library/Logs/Unreal Engine/LocalBuildLogs/*"
