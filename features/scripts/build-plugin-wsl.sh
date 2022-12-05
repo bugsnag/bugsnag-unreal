76c5ef38cd08786887ff06a9a62b0b1e4131cc94
Josh <46817760+joshedney@users.noreply.github.com>
2022-12-05 21:27:31 +0000

    Add Unreal Engine 5.1 to Windows CI machines (#199)
    * add 5.1 to windows

--- features/scripts/build-plugin-wsl.sh
+++ features/scripts/build-plugin-wsl.sh
@@ -8,9 +8,15 @@ UE_HOME="${UE_HOME:-C:\Program Files\Epic Games}"\\UE_$UE_VERSION
 
 UE_RUNUAT="$UE_HOME\Engine\Build\BatchFiles\RunUAT.bat"
 
+if [[ "$UE_VERSION" == "5.1" ]]; then
+  TARGET_PLATFORMS="Win64"
+else
+  TARGET_PLATFORMS="Win32+Win64"
+fi
+
 SCRIPT_DIR=$(pwd)
 
-/mnt/c/windows/system32/cmd.exe /C "$UE_RUNUAT" BuildPlugin -Plugin="$(wslpath -w .)/plugins/Bugsnag/Bugsnag.uplugin" -Package="$(wslpath -w .)/Build/Plugin/Bugsnag" -TargetPlatforms=Win32+Win64
+/mnt/c/windows/system32/cmd.exe /C "$UE_RUNUAT" BuildPlugin -Plugin="$(wslpath -w .)/plugins/Bugsnag/Bugsnag.uplugin" -Package="$(wslpath -w .)/Build/Plugin/Bugsnag" -TargetPlatforms="$TARGET_PLATFORMS"
 
 cd "$SCRIPT_DIR/Build/Plugin"
 
