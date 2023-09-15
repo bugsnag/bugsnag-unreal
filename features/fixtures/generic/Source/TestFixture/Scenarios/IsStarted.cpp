8fc3c8f3137641e307208c3371fec50d83b3e308
Christian <chrustuanr15@gmail.com>
2023-09-15 17:28:37 +0100

    updated test features

---
+++ features/fixtures/generic/Source/TestFixture/Scenarios/IsStarted.cpp
@@ -0,0 +1,21 @@
+#include "Scenario.h"
+
+class IsStarted : public Scenario
+{
+public:
+
+	void Run() override
+	{
+        if(Configuration.GetIsStarted())
+        {
+            UBugsnagFunctionLibrary::Notify(TEXT("isStarted"), TEXT("isStarted"));
+        }
+        else 
+        {
+            UBugsnagFunctionLibrary::Notify(TEXT("isNotStarted"), TEXT("isNotStarted"));
+        }
+		
+	}
+};
+
+REGISTER_SCENARIO(IsStarted)
