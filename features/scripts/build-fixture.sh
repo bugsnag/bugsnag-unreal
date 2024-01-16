d54bc442a81e92de477d6f886c5241c2d80da847
Josh Edney <josh.edney@smartbear.com>
2024-01-16 16:52:45 +0000

    switch between modern and legacy xcode build for unreal engine 5.3.2 and mac and ios

--- features/scripts/build-fixture.sh
+++ features/scripts/build-fixture.sh
@@ -15,8 +15,16 @@ UE_RUNUAT="${UE_HOME}/Engine/Build/BatchFiles/RunUAT.sh"
 
 echo "--- Installing plugin"
 
-unzip -o "Build/Plugin/Bugsnag-$(cat VERSION)-$(git rev-parse --short=7 HEAD)-UE_${UE_VERSION}-macOS.zip" -d features/fixtures/generic/Plugins
+#unzip -o "Build/Plugin/Bugsnag-$(cat VERSION)-$(git rev-parse --short=7 HEAD)-UE_${UE_VERSION}-macOS.zip" -d features/fixtures/generic/Plugins
 
+case "${UE_VERSION}" in
+  5.3)
+    if [[ "$PLATFORM" == "Mac" ]]; then
+      echo "--- Enabling Modern Xcode Build"
+      sed -i '' 's/bUseModernXcode=False/bUseModernXcode=True/' features/fixtures/generic/Config/DefaultEngine.ini
+    fi
+  ;;
+esac
 
 echo "--- Building Editor dependencies"
 
