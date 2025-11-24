c3ff3bb36dd092c16b1fb5383014ec1aac2a4f6b
Josh <j.edney17@gmail.com>
2025-11-24 10:39:00 +0000

    update how we get java from the environment

--- features/fixtures/generic/Source/TestFixture.Target.cs
+++ features/fixtures/generic/Source/TestFixture.Target.cs
@@ -1,16 +1,21 @@
 using UnrealBuildTool;
+using System.Collections.Generic;
 
 public class TestFixtureTarget : TargetRules
 {
-	public TestFixtureTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
+    public TestFixtureTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        bUseUnityBuild = true;
+        ExtraModuleNames.AddRange(new string[] { "TestFixture" });
 
-		DefaultBuildSettings = BuildSettingsVersion.Latest;
-
-		bEnforceIWYU = true;
-		bUseUnityBuild = true;
-
-		ExtraModuleNames.AddRange( new string[] { "TestFixture" } );
-	}
+        if (Target.Platform == UnrealTargetPlatform.IOS ||
+            Target.Platform == UnrealTargetPlatform.Mac ||
+            Target.Platform == UnrealTargetPlatform.Linux)
+        {
+            // Correct: append to string with spaces
+            AdditionalCompilerArguments += " -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer";
+        }
+    }
 }
