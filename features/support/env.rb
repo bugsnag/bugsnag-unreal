e5f0da4e87521cf94abe4dfb7e875b92014c5b74
Josh <j.edney17@gmail.com>
2025-11-25 08:45:49 +0000

    skip Crash after setting optional configuration options on ios 18/unreal engine 5.7

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,6 +22,10 @@ Before('@skip_ios') do |_scenario|
   skip_this_scenario("Not compatible with iOS") if is_platform? 'iOS'
 end
 
+Before('@skip_ios_18') do |_scenario|
+  skip_between('ios', 18, 18.99)
+end
+
 def is_platform? name
   # case-insensitive string compare, also accepts symbols. examples:
   #
