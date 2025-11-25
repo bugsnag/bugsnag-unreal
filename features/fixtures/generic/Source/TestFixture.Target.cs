a6efcf37ca86c31e1d87e7cc36c42eaad0f78567
Josh <j.edney17@gmail.com>
2025-11-25 11:43:21 +0000

    revert clang options

--- features/fixtures/generic/Source/TestFixture.Target.cs
+++ features/fixtures/generic/Source/TestFixture.Target.cs
@@ -1,24 +1,16 @@
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class TestFixtureTarget : TargetRules
 {
-    public TestFixtureTarget(TargetInfo Target) : base(Target)
-    {
-        Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.Latest;
-        bUseUnityBuild = true;
-        ExtraModuleNames.AddRange(new string[] { "TestFixture" });
+	public TestFixtureTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
 
-        // Allow modifying build environment for this target
-        bOverrideBuildEnvironment = true;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 
-        if (Target.Platform == UnrealTargetPlatform.IOS ||
-            Target.Platform == UnrealTargetPlatform.Mac ||
-            Target.Platform == UnrealTargetPlatform.Linux)
-        {
-            // Append Clang flags (string, not List)
-            AdditionalCompilerArguments += " -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer";
-        }
-    }
+		bEnforceIWYU = true;
+		bUseUnityBuild = true;
+
+		ExtraModuleNames.AddRange( new string[] { "TestFixture" } );
+	}
 }
